cb4f1e4ade7f906c9c60d2ef3f49b976e65584c1
kelly_k <pinkflamingo681@gmail.com>
2024-12-07 14:27:27 -0500

    added ability to manage list for user

---
+++ manageFood/src/App.js
@@ -0,0 +1,30 @@
+import { React, useState } from "react";
+import NewFoodInput from "./NewFoodInput";
+import Food from "./Food1";
+import "./styles.css";
+
+export default function App() {
+  const [foodsToTry, setFoodsToTry] = useState([]);
+
+  const addFoodItem = (food) => {
+    setFoodsToTry([...foodsToTry, food]);
+  };
+
+  const deleteFood = (foodToDelete) => {
+    const updatedFoods = foodsToTry.filter((food) => food !== foodToDelete);
+    setFoodsToTry(updatedFoods);
+  };
+  return (
+    <div className="App">
+      <h1>New Foods to Try</h1>
+      <NewFoodInput addFoodItem={addFoodItem} />
+      <ul className="food-list">
+        {foodsToTry.map((food, index) => (
+          <Food key={index} food={food} deleteFood={deleteFood} />
+          // You could also use this if you prefer not to create a separate function:
+          // <Food key={index} food={food} deleteFood={() => setFoodsToTry(foodsToTry.filter(foodToDelete => foodToDelete !== food))} />
+        ))}
+      </ul>
+    </div>
+  );
+}
